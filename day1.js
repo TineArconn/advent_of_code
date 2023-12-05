2d026e44ce23c2eba4d03c605cc58b34a5a14ca8
TineArconn <tinearconn@DESKTOP-1ECMB53>
2023-12-05 13:50:24 +0100

    Day 1

---
+++ day1.js
@@ -0,0 +1,33 @@
+import fs from "fs";
+
+let total = 0;
+let input = fs.readFileSync("input1.txt", { encoding: "utf8" }).split("\n");
+
+// Part 2 ---
+// Keep first and last letter (case "eightwo")
+const dict = [
+  ["one", "o1e"],
+  ["two", "t2o"],
+  ["three", "t3e"],
+  ["four", "f4r"],
+  ["five", "f5e"],
+  ["six", "s6x"],
+  ["seven", "s7n"],
+  ["eight", "e8t"],
+  ["nine", "n9e"],
+];
+
+for (let i of dict) {
+  input = input.map((e) => e.replaceAll(i[0], i[1]));
+}
+// ----------
+
+input.map((e, i) => {
+  const first = e.match(/^[a-z]*(?<first>[0-9])/);
+  const last = e.match(/(?<last>[0-9])[a-z]*$/);
+  const number = Number(first.groups.first + last.groups.last);
+  total += number;
+  console.log(i, "-", e, ":", number);
+});
+
+console.log("\n", "Total :", total);
